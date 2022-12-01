db22ea57fb4983231aa7d612309f380aad36be0a
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-12-01 09:54:08 +0100

    generating card component for displying card to the side of cardform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CredtFormComponent } from './credt-form/credt-form.component';
 import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CredtFormComponent,
-    InputComponent
+    InputComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
